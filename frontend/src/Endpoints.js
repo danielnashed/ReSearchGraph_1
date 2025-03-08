24ea641d641cb0e0f99cb26c5f033d23ffb4052d
Daniel Nashed <daniel13sr@hotmail.com>
2025-03-08 12:14:13 -0500

    update endpoint

--- frontend/src/Endpoints.js
+++ frontend/src/Endpoints.js
@@ -2,7 +2,8 @@
 // const API_BASE_URL = 'http://52.70.122.43:8003/';
 // const API_BASE_URL =  'https://65wc0j31f7.execute-api.us-east-1.amazonaws.com/prod/';
 // const API_BASE_URL = 'https://yaq1t4w8oj.execute-api.us-east-1.amazonaws.com/';
-const API_BASE_URL = 'https://2rjo6qgqsf.execute-api.us-east-1.amazonaws.com/';
+// const API_BASE_URL = 'https://2rjo6qgqsf.execute-api.us-east-1.amazonaws.com/';
+const API_BASE_URL = 'https://29zqbbn5xh.execute-api.us-east-1.amazonaws.com/';
 
 export const API_ENDPOINTS = {
     POST_LOGIN: API_BASE_URL + 'users/login/',
